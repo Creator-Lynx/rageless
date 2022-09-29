f61f1ccb544d292cf4ca0cdc62870fea5652da2f
Creator-Lynx <1_linx_1@mail.ru>
2022-09-29 16:54:48 +0300

    added in-menu presenting current quality level and its saving in player prefs

--- Assets/MainMenu/Scripts/OptionsMenu.cs
+++ Assets/MainMenu/Scripts/OptionsMenu.cs
@@ -9,22 +9,31 @@ public class OptionsMenu : MonoBehaviour
     {
         postProcessIsOn = PlayerPrefs.GetInt("PostProcessing", 1) == 1;
         ppToggle.isOn = postProcessIsOn;
+        OnClickQualityLevel(PlayerPrefs.GetInt("QualityLevel", 0));
+    }
+    //=======================================================================================================
+    [SerializeField]
+    GameObject[] qualityLevelMarkers = new GameObject[3];
+    public void OnClickQualityLevel(int level)
+    {
+        if (level > 2 || level < 0) Debug.LogError("Quality settigs level out of range!");
+        for (int i = 0; i < qualityLevelMarkers.Length; i++)
+        {
+            if (i == level)
+            {
+                qualityLevelMarkers[i].SetActive(true);
+            }
+            else
+            {
+                qualityLevelMarkers[i].SetActive(false);
+            }
+        }
+        if (QualitySettings.GetQualityLevel() == level) return;
+        QualitySettings.SetQualityLevel(level);
+        PlayerPrefs.SetInt("QualityLevel", level);
     }
 
-    public void OnClickLow()
-    {
-        QualitySettings.SetQualityLevel(0);
-    }
 
-    public void OnClickMid()
-    {
-        QualitySettings.SetQualityLevel(1);
-    }
-
-    public void OnClickHigh()
-    {
-        QualitySettings.SetQualityLevel(2);
-    }
 
     //===================================================================================================
     [SerializeField]
